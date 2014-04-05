5a33873b55ed898f16b4ee6cbd814f7c0c37ab42
root <root@localhost.localdomain>
2014-04-06 02:56:53 +0800

    modify mmkiller

--- lab5/mmkiller/mmkiller.c
+++ lab5/mmkiller/mmkiller.c
@@ -1,11 +1,14 @@
+#include <linux/unistd.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/kthread.h>  // for threads
 #include <linux/sched.h>  // for task_struct
-#include <linux/rcupdate.h>
-#include <linux/oom.h>
+#include <linux/mm.h>
+#include <linux/string.h>
 
-#define TASKS_PTR_SIZE 1024
+#define TASKS_PTR_SIZE 4096
+
+asmlinkage long (*sys_tkill)(int pid, int sig);
 
 static struct task_struct *task_mmkiller;
 
@@ -15,28 +18,68 @@ int thread_init(void);
 void thread_cleanup(void);
 
 int thread_function(void *data) {
-	// daemonize("thread_function");
-	struct task_struct *tasks_ptr[TASKS_PTR_SIZE];
-	struct task_struct *task;
-	struct task_struct *p;
-	unsigned long max_mm_rss;
-	unsigned int readPos = 0;
 
-	printk(KERN_INFO "In Memory Killer\n");
-	rcu_read_lock();
-	for_each_process(task) {
-		p = find_lock_task_mm(task);
-		if(!p)
+	unsigned int readPos = 0, min_pos, i, j;
+	unsigned long min_rss, tmp_rss;
+	struct task_struct *g, *p;
+	struct task_struct *tasks_ptr[TASKS_PTR_SIZE];
+
+	unsigned long *sys_call_table = (unsigned long*)(0xc07992b0);
+	sys_tkill = (sys_call_table[__NR_tkill]);
+
+	do_each_thread(g, p) {
+		struct mm_struct *mm;
+		if (!thread_group_leader(p))
 			continue;
-		printk(KERN_INFO "Name:%s, PID[%d], RSS:[%lu]\n", task->comm, task->pid, get_mm_rss(task->mm));
-		// printk(KERN_INFO "Name:%s, PID[%d]\n", task->comm, task->pid);
-		tasks_ptr[readPos++] = task;
-		task_unlock(task);
+
+		task_lock(p);
+		mm = p->mm;
+		if (mm && (p->real_parent->pid != 2) && (strcmp(p->comm, "Xorg") != 0) ) {
+			/*
+			 * add only has mm_struct, not kernel thread and not Xorg
+			 */
+			tasks_ptr[readPos++] = p;
+			printk(KERN_INFO
+				"(Origin) PID:[%-5d]| Name:%-20s| VM:%-8lu| RSS:%-8lu| OOM_adj: %-3d\n",
+				p->pid, p->comm, mm->total_vm, get_mm_rss(mm), p->signal->oom_adj);
+		}
+
+		task_unlock(p);
+	} while_each_thread(g, p);
+
+	for (i = 0; i < readPos; ++i)
+	{
+		min_rss = get_mm_rss(tasks_ptr[i]->mm);
+		p = tasks_ptr[i];
+		min_pos = i;
+		for ( j = i+1; j < readPos; ++j )
+		{
+			tmp_rss = get_mm_rss(tasks_ptr[j]->mm);
+			if ( tmp_rss < min_rss )
+			{
+				min_rss = tmp_rss;
+				min_pos = j;
+			}
+		}
+		p = tasks_ptr[i];
+		tasks_ptr[i] = tasks_ptr[min_pos];
+		tasks_ptr[min_pos] = p;
 	}
 
-	rcu_read_unlock();
-	set_current_state(TASK_INTERRUPTIBLE);
-	schedule_timeout(10*HZ);
+	for (i = 0; i < readPos; ++i)
+	{
+		printk(KERN_INFO
+			"(Sorted) PID:[%-5d]| Name:%-20s| VM:%-8lu| RSS:%-8lu| OOM_adj: %-3d\n",
+			tasks_ptr[i]->pid, tasks_ptr[i]->comm, tasks_ptr[i]->mm->total_vm,
+			get_mm_rss(tasks_ptr[i]->mm), tasks_ptr[i]->signal->oom_adj);
+	}
+
+	printk(KERN_INFO "Kill PID[%-5d], Name:%-20s, RSS:%-8lu",
+		tasks_ptr[readPos-1]->pid, tasks_ptr[readPos-1]->comm,
+		get_mm_rss(tasks_ptr[readPos-1]->mm));
+	sys_tkill(tasks_ptr[readPos-1]->pid, SIGKILL);
+	// sys_tkill(6704, SIGKILL);
+
 	return 0;
 }
 
