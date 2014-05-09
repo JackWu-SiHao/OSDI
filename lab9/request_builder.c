84f914d514b150a1b36b8b17fb7ab9b64b752d36
JackWu-SiHao <shwu.cs98@gmail.com>
2014-05-10 01:15:34 +0800

    modify request builder/add test pattern

--- lab9/request_builder.c
+++ lab9/request_builder.c
@@ -9,6 +9,7 @@ static struct block_device *bdev;
 static int __init init_read(void)
 {
     int i=0;
+    bool isAscending = true;
     printk("init!!!\n");
     // #define MKDEV(ma,mi)    (((ma) << MINORBITS) | (mi))
     bdev = open_by_devnum(MKDEV(8,17), 0x08000);
@@ -19,9 +20,32 @@ static int __init init_read(void)
         printk("set block size error\n");
         return -EIO;
     }
+
     // Design your write pattern here!!
-    for(i=0; i<100; i++){
-            __breadahead(bdev, i*4096, set_size);
+    for(i=1; i<=24; i++){
+
+        /* if the last request in a request group */
+        if(i%4 == 0) {
+            if(isAscending) {
+                isAscending = false;
+                __breadahead(bdev, (i*4)*512, set_size);
+
+            }
+            else {
+                isAscending = true;
+                __breadahead(bdev, ((24-i)*4-1)*512, set_size);
+            }
+        }
+
+        /* not the last request in a request group*/
+        else {
+            if(isAscending) {
+                __breadahead(bdev, (i*2)*512, set_size);
+            }
+            else {
+                __breadahead(bdev, ((24-i)*2-1)*512, set_size);
+            }
+        }
     }
     return 0;
 }
