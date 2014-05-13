8b9d7ca01104ebbe8e145c1d697a556efe19248f
JackWu-SiHao <shwu.cs98@gmail.com>
2014-05-14 02:49:08 +0800

    fix/compile error, warning

--- lab9/noop-iosched.c
+++ lab9/noop-iosched.c
@@ -86,9 +86,9 @@ static void noop_add_request(struct request_queue *q, struct request *rq)
     struct request *req = NULL;
     struct request *req_tmp = NULL;
     struct request *chosen_req = NULL;
-    struct request *min_req;
+    struct request *min_req = NULL;
     unsigned long long pos_diff;
-    unsigned long long prev_rq_pos;
+    unsigned long long prev_rq_pos = 0;
     unsigned int queue_size = 0, i;
     unsigned int curr_min = MAX_POS;
     bool can_move = false;
@@ -110,7 +110,7 @@ static void noop_add_request(struct request_queue *q, struct request *rq)
     /* find first request with mininum pos to the last time's dispathed request */
     if(!list_empty(&nd->queue)) {
         /* default min_req set to first entry of queue */
-        min_req = list_entry(&nd->queue->next, struct request, queuelist);
+        min_req = list_entry(&nd->queue, struct request, queuelist);
 
         list_for_each_entry(req, &nd->queue, queuelist) {
             if( get_diff_abs(blk_rq_pos(req), last_rq_pos) < curr_min ) {
@@ -141,7 +141,7 @@ static void noop_add_request(struct request_queue *q, struct request *rq)
             /* set default value */
             can_move = false;
             curr_min = MAX_POS;
-            chosen_req = list_entry(&nd->queue->next, struct request, queuelist);
+            chosen_req = list_entry(&nd->queue, struct request, queuelist);
 
             list_for_each_entry(req, &nd->queue, queuelist) {
 
@@ -164,7 +164,7 @@ static void noop_add_request(struct request_queue *q, struct request *rq)
 
             /* should not enter this */
             if(!chosen_req) {
-                chosen_req = list_entry(&nd->queue.next, struct request, queuelist);
+                chosen_req = list_entry(&nd->queue, struct request, queuelist);
                 printk(KERN_INFO "Error chosen_req:%-5llu\n", blk_rq_pos(chosen_req));
                 BUG_ON(1);
             } else {
