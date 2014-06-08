148efd4eb3e4fa6b418bbafffdbb537be2827f3f
JackWu-SiHao <shwu.cs98@gmail.com>
2014-06-08 21:51:15 +0800

    init add-3/will implement here tcp_ipv4

---
+++ lab12/may_use_function.c
@@ -0,0 +1,15 @@
+struct sock *tcp_check_req(struct sock *sk, struct sk_buff *skb,
+               struct request_sock *req,
+               struct request_sock **prev);
+
+    inet_csk_reqsk_queue_is_full(const struct sock *sk);
+
+    inet_csk_reqsk_queue_hash_add(struct sock *sk, struct request_sock *req,
+         unsigned long timeout);
+
+    inet_csk_reqsk_queue_drop(struct sock *sk, struct request_sock *req,
+        struct request_sock **prev);
+
+        inet_csk_reqsk_queue_unlink(sk, req, prev);
+        inet_csk_reqsk_queue_removed(sk, req);
+        reqsk_free(req);
